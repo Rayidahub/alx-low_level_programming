da71c60af02da737c66053480d5585858aa67c61
Raymond Gaius Y <rayidatech@gmail.com>
2022-10-13 07:55:26 -0700

    yoo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 /**
- *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    */
+ * main - entry.
+ * Return: Always 0.
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
