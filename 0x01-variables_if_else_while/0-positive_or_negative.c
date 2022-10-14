fd42c6299d88e7bcd7799aa2afc9ea8fe3350886
Raymond Gaius Y <rayidatech@gmail.com>
2022-10-14 09:45:59 -0700

    new file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry
+ * Return: 0 on success
+ */
+int main(void)
+
+{
+
+		int n;
+
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
+		if (n > 0)
+	{
+		printf("%d is %s\n", n, "is positive");
+	}
+		else if (n == 0)
+	{
+		printf("%d is %s\n", n, "is zero");
+	}
+		else if (n < 0)
+	{
+		printf("%d is %s\n", n, "negative");
+	}
+		return (0);
+}
