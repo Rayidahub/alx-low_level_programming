cd7efc38b1f07da4aa04cd51fe506385587d8ba3
Raymond Gaius Y <rayidatech@gmail.com>
2022-10-14 10:35:42 -0700

    last try

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,8 +5,7 @@
  * main - Determine if the last digit of a random number is
  * greater than or less than 5, or is zero..
 */
-int main(viod)
-
+int main(void)
 {
 	int n;
 	char last[] = "Last digit of";
