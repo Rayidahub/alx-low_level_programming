72ecbb912cc976446284476d7b1245bedc0fdacd
Raymond Gaius Y <rayidatech@gmail.com>
2022-10-18 10:15:33 -0700

    hm

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+/**
+* print_alphabet - prints the english alphabet from a-z.
+* Return: Nothing.
+*/
+
+void print_alphabet(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+}
+	putchar('\n');
+}
