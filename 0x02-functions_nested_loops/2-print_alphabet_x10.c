a450fd8093e1ca244d36df8b986bb864cfb4a498
Raymond Gaius Y <rayidatech@gmail.com>
2022-10-18 13:21:44 -0700

    thans

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 
+ * print_alphabet_x10  - prints alpha 10 times
  */
-void print_alphabet_10(void)
+void print_alphabet_x10(void)
 {
 	int ten;
 	char la;
