25b162c6721216268ab2dc039ca28598e0455c6a
Raymond Gaius Y <rayidatech@gmail.com>
2022-10-18 09:20:29 -0700

    perfect

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - enrty
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;	
+
+	for (i = 0; i < 9; i++)
+		{
+		putchar(c[i]);
+		}
+	putchar('\n');
+	return (0);
+}
