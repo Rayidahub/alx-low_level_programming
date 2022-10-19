745e8db043ab3211861d8da8100e1ac17acc40f9
Raymond Gaius Y <113607864+Rayidahub@users.noreply.github.com>
2022-10-19 10:27:59 +0100

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,13 @@
 #include "main.h"
-
 /**
  * print_last_digit - prints the last digit of a numer.
  * @n: digit to find the last place of.
  * Return: The last digit.
  */
-
 int print_last_digit(int n)
-
 {
-	int ld = n % 10;
-
-	if (ld < 0)
-		ld *= -1;
-	_putchar(ld + '0');
-	return (0);
+	if (n < 0)
+		n = n * -1;
+	_putchar((n % 10) + '0');
+	return (n % 10);
 }
