7c866841a888287377a96b62a6f1d6a4a6a384cd
Raymond Gaius Y <rayidatech@gmail.com>
2022-10-26 18:51:12 +0000

    sdsd git commit -m sdsd

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,10 +1,18 @@
 #include "main.h"
-/**
+ /**
  * main - infinity number.
  * Return: 0 always
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 {
-
+	char *n = i;
+	char *n1[100];
+	char *n2[10];
+/**
+ * @n1: value for first number.
+ * @n2: value for second number.
+ * @r: beffer funciton to store result.
+ * @size_r: to store buffer size.
+ */
 	return (0);
 }
