99108fec04efd5ad34a01f983ce1b7ea12ff9b27
Raymond Gaius Y <rayidatech@gmail.com>
2022-10-27 11:10:38 +0000

    2 files added

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,7 +9,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 	char *n1[100];
 	char *n2[10];
 /**
- * @n1: value for first number.
+ *@n1: Number one.
  * @n2: value for second number.
  * @r: beffer funciton to store result.
  * @size_r: to store buffer size.
